11b3c2fc9d8bce2fe444a957bbd535e235f09b32
Schubvirus <schubvirus@protonmail.com>
2023-07-12 19:18:40 +0200

    Remove Template Alert

--- src/server/create.server.ts
+++
@@ -1,9 +0,0 @@
-import { ReplicatedStorage } from "@rbxts/services";
-
-const Folder = ReplicatedStorage.WaitForChild("GuiEvents") as Folder;
-
-const createAlert = Folder.WaitForChild("createAlertBinding") as BindableEvent;
-
-wait(5);
-
-createAlert.Fire("all", "Lorem ipsum", "Emergency Alert", "Lorem ipsum");
